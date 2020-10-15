4f326f54229d171ced417e5bb1ffd2293fcd4aca
tianhuizhou <tianhuizhou@users.noreply.github.com>
2020-10-14 21:28:44 -1000

    change map
    switch to plotly mapbox instead of using leaflet

--- myjscript3.js
+++ myjscript3.js
@@ -1,99 +1,62 @@
-Promise.all([
-  d3.csv("Cholera/choleraDeathLocations.csv"),
-  d3.csv("Cholera/choleraPumpLocations.csv"),
-]).then(function(files) {
+Plotly.d3.csv('Cholera/choleraPumpLocations.csv', function (err, data_pump) {
+  Plotly.d3.csv('Cholera/choleraDeathLocations.csv', function (err, data_death) {
+    let pump_latitude = _.map(_.pluck(data_pump, 'lati'), function (x) {
 
-  console.log("js3 lalalala");
-  let xSet = _.map(_.pluck(files[1], 'long'),function (x) {
+      return x
+    });
+    let pump_longitude = _.map(_.pluck(data_pump, 'long'), function (x) {
 
-    return x
-  });
+      return x
+    });
+    let death_latitude = _.map(_.pluck(data_death, 'lati'), function (x) {
 
-  let ySet = _.map(_.pluck(files[1], 'lati'),function (y) {
+      return x
+    });
+    let death_longitude = _.map(_.pluck(data_death, 'long'), function (x) {
 
-    return y
-  });
-//read / organize data
+      return x
+    });
+    let death_num = _.map(_.pluck(data_death, 'num'), function (x) {
+      x = parseInt(x)
+      return x
+    });
 
-  console.log(xSet);
-  console.log(ySet);
+    var data = [
+      {
+        type: "scattermapbox",
+        name: "Pump",
+        lat: pump_latitude,
+        lon: pump_longitude,
+        marker: {
+          color: "#0099EE",
+          size: 16,
+        },
+      },
+      {
+        type: "scattermapbox",
+        name: "Death",
+        text: _.map(death_num, (num) => ('Death: '+ num)),
+        lat: death_latitude,
+        lon: death_longitude,
+        marker: {
+          color: "#EE0000",
+          opacity: 0.9,
+          size: _.map(death_num, (num) => (8.5 * Math.sqrt(num)))}
+      }
+    ];
 
-  let numbers = _.map(_.pluck(files[0], 'num'),function (n) {
+    var layout = {
+      dragmode: "zoom",
+      mapbox: { style: "stamen-terrain", center: { lat: 51.513, lon: -0.137 }, zoom: 14.5 },
+      margin: { r: 0, t: 0, b: 0, l: 0 },
+      showlegend:true,
+      legend: {
+        x: 0,
+        xanchor: 'left',
+        y: 0.9
+      }
+    };
 
-    return n
-  });
-
-  let xSet2 = _.map(_.pluck(files[0], 'long'),function (x) {
-
-    return x
-  });
-
-  let ySet2 = _.map(_.pluck(files[0], 'lati'),function (y) {
-
-    return y
-  });
-
-
-
-//implement leaflet map
-  var mymap = L.map('mapid').setView([51.510, -0.135], 13);
-  L.tileLayer('https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=PrivuiGzyFky7CGnwdRr',{
-    attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'
-  }).addTo(mymap);
-
-  for(let a = 0; a < xSet.length; a++){
-    L.marker([ySet[a], xSet[a]]).bindPopup("<b>Hello!</b><br>I am a Pump.").openPopup().addTo(mymap);
-  }
-
-  for(let a = 0; a < numbers.length; a++){
-    var numColor = "a"
-    if(numbers[a] < 5){
-      numColor = "#FD8D3C"
-    } else if (numbers[a] >= 5 && numbers[a] < 11){
-      numColor = "#E31A1C"
-    } else {
-      numColor = "#800026"
-    }
-    L.circle([ySet2[a], xSet2[a]], {
-      color: numColor,
-      fillColor: numColor,
-      fillOpacity: 1,
-      radius: numbers[a]/1.52
-    }).bindPopup("Total Death: " + numbers[a]).addTo(mymap);
-  }
-
-  var legend = L.control({position: 'bottomright'});
-
-
-
-  legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 5, 10],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-
-
-      div.innerHTML +=
-          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-          grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-  };
-
-  legend.addTo(mymap);
-
-
-}).catch(function(err) {
-  // handle error here
-  console.log("error")
-})
-
-function getColor(d) {
-  return d > 10 ? '#800026' :
-      d > 5  ? '#E31A1C' :
-          '#FD8D3C';
-}
+    Plotly.newPlot("mymap", data, layout, { showLink: false });
+  })
+});
